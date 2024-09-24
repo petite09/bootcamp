a1991ed436e4b9bba24feb8aed02b989bbbb62ce
Eva Durán <petiite.09@gmail.com>
2024-09-24 18:50:07 -0300

    V2

--- Ejercicio11.py
+++ Ejercicio11.py
@@ -1,1 +1,14 @@
 #Escribe un programa que cuente cuántas vocales hay en una cadena dada.
+
+def contar_vocales(cadena):
+    vocales = "aeiouAEIOUáéíóúÁÉÍÓÚ"
+    contador = 0
+    
+    for letra in cadena:
+        if letra in vocales:
+            contador +=1
+    
+    return contador
+
+cadena = input("Ingresa tu cadena: ")
+print ("El número de vocales es:", contar_vocales(cadena))
