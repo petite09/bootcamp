45be9c267b946867993fb6c4cdebc02aa4dd8f86
Eva Durán <petiite.09@gmail.com>
2024-09-24 21:43:25 -0300

    V2

--- Ejercicio15.py
+++ Ejercicio15.py
@@ -1,2 +1,5 @@
 # Escribe un programa que imprima la tabla de multiplicar del 1 al 10 para un número dado por el usuario.
 
+num = int(input("Ingresa un número: "))
+for i in range (1,11): #No incluye el 11, llega hasta el 10
+    print(f"{num} x {i} = {num * i}")
--- Ejercicio17.py
+++ Ejercicio17.py
@@ -1,1 +1,3 @@
 # Escribe un programa que tome un número entero y calcule la suma de sus dígitos.
+
+num = int(input("Ingresa un número"))
