e02969270fb0a23971cc657ec08506d784134358
Eva Durán <petiite.09@gmail.com>
2024-09-24 21:59:52 -0300

    Duda

--- Ejercicio17.py
+++ Ejercicio17.py
@@ -1,3 +1,7 @@
 # Escribe un programa que tome un número entero y calcule la suma de sus dígitos.
 
-num = int(input("Ingresa un número"))
+num = (input("Ingresa un número entero: "))
+suma = sum(int(digito) for digito in num)
+print("La suma de los dígitos es:", suma)
+
+#No entiendo en qué parte se define digito 
